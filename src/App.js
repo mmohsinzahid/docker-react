e38f63458d7ac4c3ac877a8ed1bb8bb41e9334e7
Muhammad Mohsin <mohsin@zameen.com>
2019-11-20 11:24:38 +0500

    test actions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         New test
+         hello again
         </p>
         <a
           className="App-link"
